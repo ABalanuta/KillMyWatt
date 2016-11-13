6f18124dc871ae9d7a1d792fc98d3ed78c723878
ABalanuta <AliensGoo@gmail.com>
2016-11-13 23:48:05 +0000

    Created thread for posting sending the values and storing in case of network failure

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Using RPI and ADS1115 to mesure consumption and send data to EmonCMS
 
 ## Requires
  * Adafruit_Python_ADS1x15 installed
+ * python-requests
 
 ## Install
  * run ```make install```
--- killmywatt.py
+++ killmywatt.py
@@ -1,30 +1,22 @@
 #!/usr/bin/env python
 
+import os
+import sys
+import signal
+import threading
+import subprocess
 import time
 import numpy
+import requests
 
 import Adafruit_ADS1x15
-
-from subprocess import call
-from time import gmtime, strftime
+from collections import deque
 
 
-# Create an ADS1115 ADC (16-bit) instance.
-adc = Adafruit_ADS1x15.ADS1115()
 
-# Choose a gain of 1 for reading voltages from 0 to 4.09V.
-# Or pick a different gain to change the range of voltages that are read:
-#  - 2/3 = +/-6.144V
-#  -   1 = +/-4.096V
-#  -   2 = +/-2.048V
-#  -   4 = +/-1.024V
-#  -   8 = +/-0.512V
-#  -  16 = +/-0.256V
-# See table 3 in the ADS1015/ADS1115 datasheet for more info on gain.
 GAIN = 1
-DR = 64
-READINGS_PER_SECOND = numpy.int32(120)
-DELAY = numpy.float32(1.0/120) - numpy.float32(0.205/120)
+DR = 860
+READINGS_PER_SECOND = numpy.int32(DR)
 NOISE_AVG = numpy.float32(3.56)
 RATIO_TO_W = numpy.float32(1/3.33)
 
@@ -32,56 +24,70 @@ API_KEY="ef45e7b1a19b527130339fe7c984f7a0"
 URL="http://10.0.0.253:9004/input/post.json"
 
 
+def signal_handler(signal, frame):
+	print('You pressed Ctrl+C!')
+	try:
+		if cthread.stop:
+			cthread.stop()
+		if adc.stop_adc:
+			adc.stop_adc()
+	except:
+		print 'Error Stoping Curl Thread !'
+		pass
+	sys.exit(0)
 
-FD = open("reading.log", 'w')
+class CurlThread(threading.Thread):
 
+	def __init__(self):
+		threading.Thread.__init__(self)
+		self.running = True
+
+	def run(self):
+		while self.running:
+			if len(q) > 0:
+				sum = 0
+				while len(q) > 0:
+					sum = sum + q.pop()
+				url = URL+"?json={Consumption power:"+str(sum)+"}&node=ClamPi&apikey="+API_KEY
+				try:
+					r = requests.get(url, allow_redirects=True)
+					print("Send ok", sum, "W")
+				except:
+					# if failed, send later
+					print("Error Sending")
+					q.append(sum)
+					time.sleep(2.5)
+					pass
+			else:
+				time.sleep(0.5)
+
+	def stop(self):
+		self.running = False
 
 # Once continuous ADC conversions are started you can call get_last_result() to
 # retrieve the latest result, or stop_adc() to stop conversions.
-
-def get_avg_second():
-	buff_second = numpy.array(numpy.zeros(READINGS_PER_SECOND+20, dtype=numpy.int32))
-	l_time = time.time() + 1
+def get_avg_five_second():
 	b = 0
+	l_time = time.time() + 5
 	while(time.time() < l_time):
-		buff_second[b] = adc.get_last_result()
+		buff_seconds[b] = adc.get_last_result()
 		b += 1
-		time.sleep(DELAY)
-	val = (numpy.float32(numpy.sum(buff_second)/numpy.float32(b))-NOISE_AVG)*RATIO_TO_W
-	#FD.write(" "+str(val/1000.0)+" kW\n")
+	val = ( numpy.float32( numpy.sum(buff_seconds[0:b]) / numpy.float32(b) ) - NOISE_AVG) * RATIO_TO_W
 	print str(val/1000.0)+ " kW"
 	return val
 
-def get_avg_hour():
-	buff_hour = numpy.array(numpy.zeros(3600, dtype=numpy.float64))
-	l_time = time.time() + 3600
-	for i in range(3600):
-		if (i%60 == 0):
-			print "## Minute "+str(i/60)
-			FD.write("## Minute "+str(i/60)+"\n")
-			FD.flush()
-		buff_hour[i] = numpy.float64(get_avg_second()) # using 64bit floats
-	v = numpy.mean(buff_hour)	# result in kWH
-	return v
+#
+signal.signal(signal.SIGINT, signal_handler)
 
-def get_avg_day():
-	buff_day = numpy.array(numpy.zeros(24, dtype=numpy.float64))
-	for i in range(24):
-		print "### Hour "+str(i)
-		FD.write("### Hour "+str(i))
-		buff_day[i] = get_avg_hour()
-		print "# Spent "+ str(buff_day[i]) + " kWH"
-		FD.write("# Spent "+ str(buff_day[i]) + " kWH\n")
-		print buff_day
-		print "#######################################"
-	print "Total Spent: "+str(numpy.sum(buff_day)/1000.0)+ "kWH"
-	FD.write("################################\n")
-	FD.write("# Total Spent "+ str(numpy.sum(buff_day)/1000.0) + " kWH\n")
-	FD.flush()
-	FD.close()
+# Create an ADS1115 ADC (16-bit) instance.
+adc = Adafruit_ADS1x15.ADS1115()
 
+#Buffer where the values will be stored
+buff_seconds = numpy.array(numpy.zeros(READINGS_PER_SECOND*5, dtype=numpy.int32))
 
-#logging.basicConfig(filename='readings.log',level=logging.DEBUG)
+# A queue for interthread communication
+q = deque()
+
 
 # Start continuous ADC conversions on channel 0 using the previously set gain
 # value.  Note you can also pass an optional data_rate parameter, see the simpletest.py
@@ -93,23 +99,11 @@ adc.start_adc(0, gain=GAIN, data_rate=DR)
 adc.get_last_result()
 time.sleep(0.05)
 
+# Create and start the curl thread
+cthread = CurlThread()
+cthread.start()
+
 print("Int Continuous sampling")
 
 while True:
-	buff = numpy.array(numpy.zeros(5, dtype=numpy.float32))
-	for i in range(5):
-		buff[i] = get_avg_second()
-
-	call(["curl", "--data", "data={Consumption power:"+str(numpy.mean(buff))+"}", URL+"?node=ClamPi&apikey="+API_KEY])
-	#call(["curl", "--data", "data={$(cat /proc/net/wireless|grep wlan0|awk '{print \'Wifi Link Quality:"$3"0,","Wifi Signal Level:"$4 0}')}"", URL+"?node=ClamPi&apikey="+API_KEY])
-
-	#http://10.0.0.253:9004/device/set.json?id=0
-	#print("Sent")
-
-#print("Starting ", strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()))
-#get_avg_day()
-
-
-#print("Ended ", strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()))
-
-adc.stop_adc()
+	q.append(get_avg_five_second())
